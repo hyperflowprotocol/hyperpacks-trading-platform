f3453ad82f417ea5acc194f8afc3c5c0bfd1739f
hyperflowprotocol <bsslp79@gmail.com>
2025-10-03 21:57:22 +0800

    Revert to original state before agent changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Landing from './pages/Landing';
 import LaunchApp from './pages/LaunchApp';
 import Presale from './pages/Presale';
 import Docs from './pages/Docs';
-import AdminWalletChecker from './pages/AdminWalletChecker';
 import './App.css';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
       <Route path="/app" element={<LaunchApp />} />
       <Route path="/presale" element={<Presale />} />
       <Route path="/docs" element={<Docs />} />
-      <Route path="/admin/wallet-checker" element={<AdminWalletChecker />} />
     </Routes>
   );
 }
